683b64e25a1cebb88434531c16914851c7cfd84f
PRINCIPAL <PRINCIPAL@User>
2017-04-02 16:47:31 +0530

    improved

--- src/daivi/AdvertisementTest.java
+++ src/daivi/AdvertisementTest.java
@@ -14,54 +14,60 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Enclosed.class)
 public class AdvertisementTest {
 
-	@RunWith(Parameterized.class)
-	public static class RequestToAdvertiseInvalidSectionServesErrorPage {
+	@RunWith(Enclosed.class)
+	public static class RequestToAdvertiseASection {
+		
+		protected final String section;
 
-		private final String section;
-
-		@Parameters
-		public static Iterable<Object[]> data() {
-			return Arrays.asList(new Object[][] {
-				{null},
-				{""},
-				{"    "},
-				{"satSang"}
-			});
-		}
-
-		public RequestToAdvertiseInvalidSectionServesErrorPage(String section) {
+		public RequestToAdvertiseASection(String section) {
 			this.section = section;
 		}
 		
-		@Test
-		public void requestToAdvertiseInvalidSectionServesErrorPage() {
-			PageToServe pageToServe = Advertisement.handleRequestToAdvertise(section);
-			PageToServe expectedPageToServe = new PageToServe("error.html");
-			assertEquals(expectedPageToServe, pageToServe);
+		@RunWith(Parameterized.class)
+		public static class InvalidSectionServesErrorPage extends RequestToAdvertiseASection {
+			
+			@Parameters
+			public static Iterable<Object[]> data() {
+				return Arrays.asList(new Object[][] {
+					{null},
+					{""},
+					{"    "},
+					{"satSang"}
+				});
+			}
+			
+			public InvalidSectionServesErrorPage(String section) {
+				super(section);
+			}
+			
+			@Test
+			public void requestToAdvertiseInvalidSectionServesErrorPage() {
+				PageToServe pageToServe = Advertisement.handleRequestToAdvertise(section);
+				PageToServe expectedPageToServe = new PageToServe("error.html");
+				assertEquals(expectedPageToServe, pageToServe);
+			}
 		}
-	}
-	
-	@RunWith(Parameterized.class)
-	public static class RequestToAdvertiseASectionServesPageToAssistIt {
 		
-		private final String section;
-		
-		@Parameters
-		public static Iterable<Object[]> data() {
-			return Arrays.asList(new Object[][] {
-				{"satsang"}
-			});
-		}
-		
-		public RequestToAdvertiseASectionServesPageToAssistIt(String section) {
-			this.section = section;
-		}
-		
-		@Test
-		public void requestToAdvertiseInvalidSectionServesErrorPage() {
-			PageToServe pageToServe = Advertisement.handleRequestToAdvertise(section);
-			PageToServe expectedPageToServe = new PageToServe(MessageFormat.format("{0}.adassist", section));
-			assertEquals(expectedPageToServe, pageToServe);
+		@RunWith(Parameterized.class)
+		public static class ValidSectionServesPageToAssistIt extends RequestToAdvertiseASection {
+			
+			@Parameters
+			public static Iterable<Object[]> data() {
+				return Arrays.asList(new Object[][] {
+					{"satsang"}
+				});
+			}
+			
+			public ValidSectionServesPageToAssistIt(String section) {
+				super(section);
+			}
+			
+			@Test
+			public void requestToAdvertiseInvalidSectionServesErrorPage() {
+				PageToServe pageToServe = Advertisement.handleRequestToAdvertise(section);
+				PageToServe expectedPageToServe = new PageToServe(MessageFormat.format("{0}.adassist", section));
+				assertEquals(expectedPageToServe, pageToServe);
+			}
 		}
 	}
 	
