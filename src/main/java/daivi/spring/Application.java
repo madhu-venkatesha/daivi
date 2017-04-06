589f03e4de480efce1e7f4294f869ccd75710418
P A Vijaya <P A Vijaya@Vijaya>
2017-04-06 21:34:23 +0530

    some changes

--- src/main/java/daivi/spring/Application.java
+++ src/main/java/daivi/spring/Application.java
@@ -3,8 +3,10 @@ package daivi.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:daivi/spring/application.properties")
 @ImportResource("classpath:daivi/spring/application-context.xml")
 public class Application {
 
---
+++ src/main/java/daivi/spring/CustomErrorController.java
@@ -0,0 +1,21 @@
+package daivi.spring;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+  
+    private static final String PATH = "/custom_error";
+     
+    @RequestMapping(value=PATH)
+    public String error() {
+        return "error.html";
+    }
+ 
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
