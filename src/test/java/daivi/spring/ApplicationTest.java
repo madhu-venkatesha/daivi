140f2872c7fbb77a70ffa22891036c75181cc0a1
P A Vijaya <P A Vijaya@Vijaya>
2017-05-13 20:57:55 +0530

    ApplicationTest.java

---
+++ src/test/java/daivi/spring/ApplicationTest.java
@@ -0,0 +1,20 @@
+package daivi.spring;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ApplicationTest {
+
+	@BeforeClass
+	public static void init() {
+		Application.main(new String[]{});
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
